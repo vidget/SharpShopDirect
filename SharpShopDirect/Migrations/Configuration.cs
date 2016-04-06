9bff6adc8c16e6c1888275860177a2414c5f728f
Vidget <vidget@yahoo.com>
2016-04-06 10:55:46 -0400

    Commented Seed method in Configuration file
    I did this because after seeding from a local file, I didn't have to
    seed anymore, when I left it in and tried to register a user it looked
    for the local file and returned an error. Should store on server.

--- SharpShopDirect/Migrations/Configuration.cs
+++ SharpShopDirect/Migrations/Configuration.cs
@@ -18,65 +18,69 @@ namespace SharpShopDirect.Migrations
 
         protected override void Seed(SharpShopDirect.Models.ApplicationDbContext context)
         {
-            string txt = @"C:\Users\vidget\Documents\Visual Studio 2013\Projects\Fashion\Fashion\Content\database.txt";
-            List<string> DatabaseInfoList = new List<string>();
+            //Commented out after deploy because when you try to register it looks for this local file to reseed, which isn't
+            //necessary. There is probaly an easyier way to just read the file from the local server.
 
 
-            using (StreamReader readerOfCSV = new StreamReader(txt))
-            {
-                string dataLine;
-
-                while ((dataLine = readerOfCSV.ReadLine()) != null)
-                {
-                    DatabaseInfoList.Add(dataLine);
-                }
-            }
+            //string txt = @"C:\Users\vidget\Documents\Visual Studio 2013\Projects\Fashion\Fashion\Content\database.txt";
+            //List<string> DatabaseInfoList = new List<string>();
 
 
-            foreach (var line in DatabaseInfoList)
-            {
+            //using (StreamReader readerOfCSV = new StreamReader(txt))
+            //{
+            //    string dataLine;
 
-                string[] itemArray = line.Split(',');
-
-                string itemid = itemArray[0];
-                string itemType = itemArray[1];
-                string collection = itemArray[2];
-                string itemNumber = itemArray[3];
-                string itemName = itemArray[4];
-                string itemDecription = itemArray[5];
-                string itemPrice = itemArray[6];
-                string itemColor = itemArray[7];
-                string itemImage = itemArray[8];
-
-                context.Items.Add(new Item
-                {
-
-                    ItemId = Int32.Parse(itemid),
-                    ItemType = itemType,
-                    Collection = collection,
-                    ItemNumber = Int32.Parse(itemNumber),
-                    ItemName = itemName,
-                    Description = itemDecription,
-                    Price = decimal.Parse(itemPrice),
-                    Color = itemColor,
-                    Image = itemImage
-
-                });
-
-            }
+            //    while ((dataLine = readerOfCSV.ReadLine()) != null)
+            //    {
+            //        DatabaseInfoList.Add(dataLine);
+            //    }
+            //}
 
 
+            //foreach (var line in DatabaseInfoList)
+            //{
+
+            //    string[] itemArray = line.Split(',');
+
+            //    string itemid = itemArray[0];
+            //    string itemType = itemArray[1];
+            //    string collection = itemArray[2];
+            //    string itemNumber = itemArray[3];
+            //    string itemName = itemArray[4];
+            //    string itemDecription = itemArray[5];
+            //    string itemPrice = itemArray[6];
+            //    string itemColor = itemArray[7];
+            //    string itemImage = itemArray[8];
+
+            //    context.Items.Add(new Item
+            //    {
+
+            //        ItemId = Int32.Parse(itemid),
+            //        ItemType = itemType,
+            //        Collection = collection,
+            //        ItemNumber = Int32.Parse(itemNumber),
+            //        ItemName = itemName,
+            //        Description = itemDecription,
+            //        Price = decimal.Parse(itemPrice),
+            //        Color = itemColor,
+            //        Image = itemImage
+
+            //    });
+
+            //}
 
 
-            context.Blogs.Add(new Blog
-            {
-                Blogid = 0,
-                Date = DateTime.Now,
-                MyComment = "Hello, World"
 
-            });
 
-            base.Seed(context);
+            //context.Blogs.Add(new Blog
+            //{
+            //    Blogid = 0,
+            //    Date = DateTime.Now,
+            //    MyComment = "Hello, World"
+
+            //});
+
+            //base.Seed(context);
         }
     }
 }
